a664f296f75714a222262cf633cb3d7fac9f2220
Dima <dmytro.vladyka@gmail.com>
2024-10-26 11:07:31 +0300

    Create Project

---
+++ src/main/java/edu/itstep/project/EgzaminProjectTaskApplication.java
@@ -0,0 +1,13 @@
+package edu.itstep.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgzaminProjectTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EgzaminProjectTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/itstep/project/EgzaminProjectTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.itstep.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgzaminProjectTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
